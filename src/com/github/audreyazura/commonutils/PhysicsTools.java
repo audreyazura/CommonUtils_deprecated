b5b1b6dc480eefa079dc92019cbb26a65385777d
audreyazura <audreyazura@localhost>
2020-09-14 19:03:20 +0900

    Renaming the main package and suppressing other people's code

--- src/com/github/audreyazura/commonutils/PhysicsTools.java
+++ src/com/github/audreyazura/commonutils/PhysicsTools.java
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with CommonUtils.  If not, see <https://www.gnu.org/licenses/>.
  */
-package commonutils;
+package com.github.audreyazura.commonutils;
 
 import java.math.BigDecimal;
 
